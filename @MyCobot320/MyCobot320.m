3f7e136e816a3ba398d5cb0cace2a924aa6f56f3
Jack McCann <JakProjects@Jacks-MacBook-Pro-105.local>
2023-10-23 10:17:24 +1100

    Progresses on IK solver for cobot.

--- @MyCobot320/MyCobot320.m
+++ @MyCobot320/MyCobot320.m
@@ -36,20 +36,11 @@ classdef MyCobot320 < RobotBaseClass
             link(6) = Link('d',0.0655, 'a', 0, 'alpha', pi/2, 'qlim', deg2rad([-175 175]));
             
         % end effector parameters
-
-<<<<<<< HEAD
-             self.useTool = 1;
-             self.toolFilename = 'MyCobot320EndEffector1.ply';
-             self.toolTr = transl(0, 0.095, 0);
-                       
-=======
              self.useTool = 1;                                  % toggle on and off tool
              self.toolFilename = 'MyCobot320EndEffector1.ply';  %ply file that replaces final link
              self.toolTr = transl(0, 0.095, 0);                 % tip of the end effector                   
            
-            
->>>>>>> 06a4e811960aa3d39c5e05afa77b7cba44cb5b3d
-            self.model = SerialLink(link,'name',self.name);
+             self.model = SerialLink(link,'name',self.name);
         end
     end
 end
