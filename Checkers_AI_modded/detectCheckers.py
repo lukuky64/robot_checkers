841ca93883c27b533b9b6a9e4f9134ebfe20bd82
Lucas Moore <lucas.j.moore@student.uts.edu.au>
2023-10-24 13:27:42 +1100

    need to fix watershed algorithm on checker image detection

--- Checkers_AI_modded/detectCheckers.py
+++ Checkers_AI_modded/detectCheckers.py
@@ -1,6 +1,3 @@
-# This class will detect the locations of the blue checkers (user is assumed to play as blue) on the board
-# given an RGB imagefeed through a ROS node. It will then publish the locations of the blue checkers to a ROS topic.
-
 import cv2
 import numpy as np
 import rospy
@@ -12,54 +9,95 @@ class CheckerboardDetector:
         self.bridge = CvBridge()
         self.image_sub = rospy.Subscriber("/usb_cam/image_raw", Image, self.callback)
         self.locations = []
+        self.process_image = False  # Flag to control when to process the image
 
     def callback(self, data):
-        try:
-            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-        except CvBridgeError as e:
-            print(e)
-            return
+        if self.process_image:
+            try:
+                cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+            except CvBridgeError as e:
+                print(e)
+                return
 
-        # Detect 8x8 checkerboard using open CV function
-        ret, corners = cv2.findChessboardCorners(cv_image, (8,8), None)
-        
-        if ret:
-            # Isolate blue channel and convert to greyscale
-            blue_channel = cv_image[:,:,0] # BGR, blue channel is index 0
-            grey_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+            ret, corners = cv2.findChessboardCorners(cv_image, (7, 7), None)
             
-            # Apply watershed algorithm
-            _, thresh = cv2.threshold(grey_image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-            sure_bg = cv2.dilate(thresh, None, iterations=3)
-            ret, sure_fg = cv2.threshold(thresh, 0.7 * thresh.max(), 255, 0)
-            unknown = cv2.subtract(sure_bg, sure_fg)
-            
-            markers = cv2.connectedComponents(sure_fg)[1]
-            markers += 1
-            markers[unknown==255] = 0
-            
-            cv2.watershed(cv_image, markers)
-            
-            # Get centre of each blue checker
-            unique_markers = np.unique(markers)
-            for marker in unique_markers:
-                if marker == 0:
-                    continue
+            if ret:
+                print("Checkerboard found!")
                 
-                coords = np.argwhere(markers == marker)
-                y, x = coords.mean(axis=0)
+                # Isolate blue channel
+                blue_channel = cv_image[:,:,0] 
                 
-                # Represent its location on the 8x8 grid
-                x_grid = int(7 * (x / cv_image.shape[1]))
-                y_grid = int(7 * (1 - (y / cv_image.shape[0])))
+                # Threshold to identify blue regions
+                _, blue_thresh = cv2.threshold(blue_channel, 128, 255, cv2.THRESH_BINARY)
                 
-                self.locations.append((x_grid, y_grid))
-            
-            print("Locations of blue checkers: ", self.locations)
-            self.locations.clear()
+                # Apply watershed algorithm on blue_thresh
+                kernel = np.ones((3,3), np.uint8)
+                opening = cv2.morphologyEx(blue_thresh, cv2.MORPH_OPEN, kernel)
+                sure_bg = cv2.dilate(opening, None, iterations=3)
+                ret, sure_fg = cv2.threshold(opening, 0.7 * opening.max(), 255, 0)
+                unknown = cv2.subtract(sure_bg, sure_fg)
+                
+                markers = cv2.connectedComponents(sure_fg)[1]
+                markers += 1
+                markers[unknown == 255] = 0
+                
+                cv2.watershed(cv_image, markers)
+
+                # cv_image[markers == -1] = [0, 0, 255]  # Marking the boundaries with red color
+                # cv2.imshow('Watershed Segmentation', cv_image)
+                # cv2.waitKey(0)
+
+
+                unique_markers = np.unique(markers)
+                
+                for marker in unique_markers:
+                    if marker <= 1:  # Skip background and boundary markers
+                        continue
+                    
+                    coords = np.argwhere(markers == marker)
+                    y, x = coords.mean(axis=0)
+                    
+                    x_grid = int(7 * (x / cv_image.shape[1]))
+                    y_grid = int(7 * (1 - (y / cv_image.shape[0])))
+                    
+                    self.locations.append((x_grid, y_grid))
+
+                    # Highlight checker in the original image
+                    for coord in coords:
+                        cv_image[coord[0], coord[1]] = [0, 255, 0]  # Green color
+                
+                # Display the modified image
+                cv2.imshow('Highlighted Checkers', cv_image)
+                cv2.waitKey(0)
+                
+                self.locations = list(set(self.locations))
+                print("Locations of blue checkers: ", self.locations)
+                self.locations.clear()
+                
+            self.process_image = False  # Reset the flag
+
 
 if __name__ == '__main__':
     rospy.init_node('checkerboard_detector', anonymous=True)
     detector = CheckerboardDetector()
-    input("Press Enter to capture")
-    rospy.spin()
+
+    try:
+        exit_flag = False
+        while True:  # Loop to wait for Enter key
+            while(not detector.process_image == True):
+                user_input = input("Press Enter to capture or type 'exit' to quit: ")
+
+                if user_input.lower() == 'exit':  # Exit the program
+                    print("Exiting...")
+                    exit_flag = True
+                    break
+                detector.process_image = True  # Set the flag to True when Enter is pressed
+            
+            if exit_flag:
+                break
+
+    except KeyboardInterrupt:
+        print("Ctrl+C detected. Shutting down.")
+
+    rospy.signal_shutdown('Exit requested')  # Shutdown the ROS node
+
