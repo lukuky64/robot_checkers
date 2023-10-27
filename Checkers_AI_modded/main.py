ca07d1154a4a68c2679894c88927065fdda68a4e
Lucas Moore <lucas.j.moore@student.uts.edu.au>
2023-10-27 16:29:04 +1100

    a

--- Checkers_AI_modded/main.py
+++ Checkers_AI_modded/main.py
@@ -1,4 +1,5 @@
 import checkers_image_interaction
+import checkers
 import gamebot
 from time import sleep
 ##COLORS##
@@ -19,7 +20,7 @@ SOUTHEAST = "southeast"
 
 def main():
     while True:
-        game = checkers_image_interaction.Game(loop_mode=False)
+        game = checkers_image_interaction.Game(loop_mode=False) # checkers_image_interaction
         game.setup()
         bot = gamebot.Bot(game, RED, mid_eval='piece_and_board',
                           end_eval='sum_of_dist', method='alpha_beta', depth=3)
