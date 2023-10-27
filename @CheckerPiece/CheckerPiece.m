74535c20bfe921bba67a357339b61476dd2f8488
Jack McCann <JakProjects@Jacks-MacBook-Pro-105.local>
2023-10-27 20:56:43 +1100

    sjkchbskjhb

--- @CheckerPiece/CheckerPiece.m
+++ @CheckerPiece/CheckerPiece.m
@@ -13,7 +13,11 @@ classdef CheckerPiece < handle
         end
 
         function plotMe(self)
-            [f,v,data] = plyread('checkerRed.ply','tri');
+            if self.colour == 'red'
+                [f,v,data] = plyread('checkerRed.ply','tri');
+            elseif self.colour == 'blue'
+                [f,v,data] = plyread('checkerBlue.ply','tri');
+            end
             vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue]/255;
             self.plotHandle = trisurf(f,v(:,1)+ self.base(1,4),v(:,2)+ self.base(2,4), ...
                 v(:,3)+ self.base(3,4),'FaceVertexCData',vertexColours, ...
